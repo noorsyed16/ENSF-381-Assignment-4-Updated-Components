c72833fe6761b2ffd3291eb7f68266fd3382d10d
noorsyed16 <noorsyed464@gmail.com>
2025-04-14 15:44:51 -0600

    Add files via upload

---
+++ EnrolledCourses.js
@@ -0,0 +1,34 @@
+import React, {useContext} from 'react';
+import courses from '../data/courses.js';
+import course1 from '../images/course1.jpg';
+import course2 from '../images/course2.jpg';
+import './EnrolledCourses.css';
+
+function EnrolledCourse(props){
+    let courseName = props.course.name;
+    let courseCreditHours = props.course.creditHours;
+    let courseImage = props.course.image;
+    
+
+    function drop(course){
+        let tempArray = []
+        for(let i = 0; i < props.enrolledCourses.length; i++){
+            if(course.id != props.enrolledCourses[i].id){
+                tempArray.push(props.enrolledCourses[i]);
+            }
+        }
+        props.setEnrolledCourses(tempArray);
+    }
+    return(
+        <td>
+        <div>
+            <h2>{courseName}</h2>
+            <img src= {courseImage ? course1 : course2} alt="Course image"></img>
+            <p>Credit Hours: {courseCreditHours}</p>
+            <button onClick={()=>drop(props.course)}>Drop</button>
+        </div>
+        </td>
+    );
+}
+
+export default EnrolledCourse;
---
+++ EnrollmentList.js
@@ -0,0 +1,81 @@
+import React, {useState, useEffect} from 'react';
+import EnrolledCourse from './EnrolledCourses';
+import './EnrollmentList.css';
+import courses from '../data/courses';
+
+function EnrollmentList({enrolledCourses, setEnrolledCourses}){
+    const [enrollmentHours, setEnrollmentHours] = useState(0);
+    
+
+    useEffect(() => {
+        // load
+        let data = [];
+        for(let i = 0; i < localStorage.length; i++){
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            data.push(value);
+            
+        setEnrolledCourses(data)
+        }
+
+    },[])
+
+    useEffect(() => {
+        let enrolledHoursCount = 0;
+        if(enrolledCourses.length > 0){
+            for(let i = 0; i < enrolledCourses.length; i++){
+                let valueToAdd = enrolledCourses[i];
+                let keyToAdd = valueToAdd.toString();
+
+                localStorage.setItem(keyToAdd, valueToAdd);
+                enrolledHoursCount += enrolledCourses[i].creditHours;
+            }
+        }
+        setEnrollmentHours(enrolledHoursCount);
+
+    },[enrolledCourses])
+
+    function renderCourse(){
+        let count = 0;
+        let rows = [];
+        let coursesToRender = [];
+        
+
+        for(let i = 0; i < enrolledCourses.length; i++){
+            let course = enrolledCourses[i]
+            //enrolledHoursCount += courses[id].creditHours;
+            coursesToRender.push(
+                <EnrolledCourse course= {course} enrolledCourses={enrolledCourses} setEnrolledCourses={setEnrolledCourses}> </EnrolledCourse>
+            )
+            count += 1;
+            if(count === 3){
+                rows.push(
+                    <tr>
+                        {coursesToRender}
+                    </tr>
+                )
+                coursesToRender = [];
+                count = 0;
+            }
+        }
+        rows.push(
+            <tr>
+                {coursesToRender}
+            </tr>
+        )
+        //setEnrollmentHours(enrolledHoursCount);
+        return rows;
+    }
+
+    return(
+        <div>
+            <h1>Enrollment List</h1>
+            <h3>Total Credit Hours: {enrollmentHours} </h3>
+            <table className = "cv">
+                {renderCourse()}
+            </table>
+        </div>
+    );
+}
+
+export default EnrollmentList;
